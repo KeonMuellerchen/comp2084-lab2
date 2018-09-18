c883746d964c5c403e106b0ee99b9a7ea0c6d2dc
Keon Muellerchen <Keonmuellerchen@gmail.com>
2018-09-18 16:34:28 -0400

    Lab 2

---
+++ Lab2/Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//reference Models namespace
+//using Lab2.Models;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Products/Details
+        public ActionResult Details(string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+        }
+    }
+}
